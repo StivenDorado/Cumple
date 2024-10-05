8ee179f13ed08bd3d4c06f23059ae0930f357234
Stiven Dorado <golds9013@gmail.com>
2024-10-05 09:30:23 -0500

    Happy Birthday Jheny_2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Olvide Decirte Algo Importante'.split('')
+const text = 'Olvide Decirte Algo'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
